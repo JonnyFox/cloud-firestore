38b6db1904ff7c5b4e2975f65fa9897054c2f6e3
jfox <fox.jonny@gmail.com>
2017-11-06 16:47:17 +0100

    add, set and update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
     selector: 'app-root',
     template: `
         <button (click)="addItem()">Add Item</button>
+        <button (click)="upsertItem()">Modify Item</button>
         <ul>
             <li *ngFor="let item of items | async">
                 <pre>{{ item | json }}</pre>
@@ -28,4 +29,20 @@ export class AppComponent {
             anotherTest: 'another value'
         });
     }
+
+    public upsertItem(): void {
+
+        // in this case the document will be added to the collection
+        this.itemCollection.doc('Document1').set({
+            value: 33,
+            content: 'Very important'
+        });
+
+        // here we will change the content field only
+        this.itemCollection.doc('Document1').update({
+            content: 'Top secret'
+        });
+    }
+
+    
 }
